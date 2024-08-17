4610e5c76249d4864bd00308a60045f95cdd040f
Lovepreet062003 <singhofficial890@gmail.com>
2024-08-17 12:58:08 +0530

    frontend

---
+++ Shop.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import Popular from '../Components/Popular/Popular'
+
+export const Shop = () => {
+  return (
+    <div>
+        <Hero/>
+        <Popular/>
+        <Offers/>
+        <NewCollections/>
+        <NewsLetter/>
+    </div>
+  )
+}
+
+export default Shop
